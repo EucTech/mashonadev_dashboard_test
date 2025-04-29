c73bbaed4087851aa26e57b104fdff2d8eccb92b
EucTech <euccreative@gmail.com>
2025-04-28 21:34:48 -0400

    updated

--- src/components/Dashboard/Sidebar.tsx
+++ src/components/Dashboard/Sidebar.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import { sidebarLinks } from "../../utilities/utlis";
 import { MdLogout } from "react-icons/md";
-import { usePathname} from "next/navigation"; 
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 import classNames from "classnames";
@@ -13,7 +13,6 @@ const Sidebar = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const pathname = usePathname();
   const activePath = pathname;
-  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
@@ -28,25 +27,23 @@ const Sidebar = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  useEffect(() => {
-    setLoading(false); 
-  }, [pathname]);
-
   const handleToggle = () => {
+    // Only toggle if screen width is >= 1024px
     if (window.innerWidth >= 1024) {
       setIsCollapsed(!isCollapsed);
     } else {
+      // Always ensure it's collapsed on smaller screens
       setIsCollapsed(true);
     }
   };
 
   const sidebarWidth = isCollapsed ? "w-16" : "w-60";
 
-  const handleLinkClick = (link: string) => {
+  const handleLinkClick = () => {
     if (window.innerWidth < 1024) {
+      // We want to collapse the sidebar after navigating
       setIsCollapsed(true);
     }
-    setLoading(true);
   };
 
   return (
@@ -56,19 +53,16 @@ const Sidebar = () => {
         sidebarWidth
       )}
     >
-      {loading && (
-        <div className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-50 flex items-center justify-center z-50">
-          <div className="loader border-4 border-t-4 border-gray-200 rounded-full w-10 h-10 animate-spin border-t-[#262A48]" />
-        </div>
-      )}
-
       <div className="flex items-center justify-between h-[96px] px-4 border-b border-[#E5E5E5]">
         {!isCollapsed && (
           <div className="text-xl font-bold text-[#262A48]"></div>
         )}
         <button
           onClick={handleToggle}
-          className="p-2 bg-[#262A48] rounded-full cursor-pointer transition-transform duration-300"
+          className={classNames(
+            "p-2 bg-[#262A48] rounded-full cursor-pointer transition-transform duration-300",
+            // { "rotate-180": isCollapsed }
+          )}
         >
           <LuPanelRight className="text-[#EBFFE2]" size={20} />
         </button>
@@ -77,7 +71,7 @@ const Sidebar = () => {
       <div className="flex-1 overflow-y-auto overflow-x-hidden mt-3">
         <div className={`flex flex-col gap-1 transition-transform duration-300 ${isCollapsed ? "px-0" : "px-6"}`}>
           {sidebarLinks.map(({ name, icon, link }) => (
-            <Link href={link} key={name} onClick={() => handleLinkClick(link)}>
+            <Link href={link} key={name} onClick={handleLinkClick}>
               <div
                 className={classNames(
                   "flex items-center gap-4 p-3 transition-all duration-300 cursor-pointer",
